0c188b7377be80c02649c3b6e37239b61fded214
jan-molak <jan.molak@gmail.com>
2018-06-14 18:02:54 +0100

    Import indicate what Tasks to use

--- features/step_definitions/steps.ts
+++ features/step_definitions/steps.ts
@@ -1,13 +1,14 @@
-import { protractor } from 'protractor';
+import { protractor, by } from 'protractor';
 import { Actor, BrowseTheWeb, Open, See, Target, Text } from 'serenity-js/lib/screenplay-protractor';
+import { expect } from '../../test/expect';
 
 export = function steps() {
 
-    this.Given(/^(.*) wants to interact with AngularJS apps$/, function(actor: string) {
+    this.Given(/^(.*) wants to interact with AngularJS apps$/, function(actorName: string) {
         return Promise.resolve('pending');
     });
 
-    this.Then(/(?:he|she|they) should have access to the (.*) playground/, function(playgroundName: string) {
+    this.Then(/(?:he|she|they) should have access to the (.*) playground/, function(epxectedPlaygroundName: string) {
         return Promise.resolve('pending');
     });
 };
